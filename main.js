4b3e63c84b1200d29dbda320aeb106279614f0aa
damian zielinski <dazielinscy@gmail.com>
2024-06-24 11:25:38 +0200

    zmiany

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-    console.log('helo');
-})
+$(document).ready(function () {
+	$('.accordion-item').click(function () {
+		// $('.accordion-content').slideDown(2000);
+
+		if ($(this).hasClass('open')) {
+			$(this).removeClass('open');
+			$(this).find('.accordion-content').slideUp(2000);
+		} else {
+			$(this).addClass('open');
+			$(this).find('.accordion-content').slideDown(3000);
+		}
+	});
+});
